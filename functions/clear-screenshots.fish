d084247bfed82459e08f9b1355e47f65752c4f31
Chirikumbrah <basslo@protonmail.com>
2023-01-20 11:49:18 +0300

    added functions for clearing screenshots dir

---
+++ functions/clear-screenshots.fish
@@ -0,0 +1,3 @@
+function clear-screenshots --wraps='trash ~/yrData/images/screenshots/*' --description 'alias clear-screenshots=trash ~/yrData/images/screenshots/*'
+  trash ~/yrData/images/screenshots/* $argv; 
+end

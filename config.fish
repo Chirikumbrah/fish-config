42395a08ca7691a8fa2e8ab9ce34c3d714777fca
Chirikumbrah <basslo@protonmail.com>
2023-01-25 20:59:10 +0300

    sxiv config replaced by nsxiv

--- config.fish
+++ config.fish
@@ -18,7 +18,7 @@ set -U fish_greeting
 set -gx LANG 'en_US.UTF-8'
 set -gx LC_ALL $LANG
 set -gx BROWSER 'chromium'
-set -gx IMAGE_VIEWER 'sxiv'
+set -gx IMAGE_VIEWER 'nsxiv'
 set -gx VIDEO_PLAYER 'ffplay'
 set -gx CM_LAUNCHER 'dmenu'
 set -gx SCRIPTS "$HOME/.scripts"

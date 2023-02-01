5e3886bb3ee72f72212e8e5f6f341e521aa6e949
Chirikumbrah <basslo@protonmail.com>
2023-02-01 10:00:12 +0300

    added rm function (like trs)

--- functions/rm.fish
+++ functions/rm.fish
@@ -1,3 +1,12 @@
-function rm --description 'alias rm=rm -i'
- command rm -i $argv; 
+function rm
+  if count $argv > /dev/null
+    if test "$argv[1]" = "$HOME/" || test "$argv[1]" = "$HOME" || test "$argv[1]" = "/"
+      fd . "$argv[1]" --hidden -tf -td --color=always | fzf --ansi -m | xargs -I {} rm -rf {}
+    else
+      rm -rf $argv
+    end
+  else
+    fd --hidden -tf -td --color=always | fzf --ansi -m | xargs -I {} rm -rf {}
+  end
 end
+

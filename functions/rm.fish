b68fccf8562ae32029028e1ffbe9bf335f491b61
Chirikumbrah <basslo@protonmail.com>
2023-03-10 23:30:31 +0300

    rm: some type changes

--- functions/rm.fish
+++ functions/rm.fish
@@ -1,12 +1,12 @@
 function rm
   if count $argv > /dev/null
     if test "$argv[1]" = "$HOME/" || test "$argv[1]" = "$HOME" || test "$argv[1]" = "/"
-      fd . "$argv[1]" --hidden -tf -a -td --color=always | fzf --ansi -m --preview '~/.scripts/system/fzf-preview.sh {}' | xargs -I {} rm -fr {}
+      fd . "$argv[1]" --hidden -a -td -tf --color=always | fzf --ansi -m --preview '~/.scripts/system/fzf-preview.sh {}' | xargs -I {} rm -fr {}
     else
       command rm -rf $argv
     end
   else
-    fd --hidden -tf -a -td --color=always | fzf --ansi -m --preview '~/.scripts/system/fzf-preview.sh {}' | xargs -I {} rm -fr {}
+    fd --hidden -a -td -tf --color=always | fzf --ansi -m --preview '~/.scripts/system/fzf-preview.sh {}' | xargs -I {} rm -fr {}
   end
 end
 

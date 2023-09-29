603625d4ff108a75074c5b1fda42b83835d4879c
Chirikumbrah <basslo@protonmail.com>
2023-09-29 12:21:33 +0300

    added thefuck functions

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,3 +1,5 @@
 function fish_user_key_bindings
-    bind \e thefuck-command-line # Bind EscEsc to thefuck
+    bind \e\e thefuck-command-line # Bind EscEsc to thefuck
+    bind \cq exit
+    bind \er reload
 end

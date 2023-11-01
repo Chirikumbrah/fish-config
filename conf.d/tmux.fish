7f77a3414a2c7aa319648770bc6a1ad3d6b511f5
Chirikumbrah <basslo@protonmail.com>
2023-11-01 23:00:59 +0300

    commented tmux startup

--- conf.d/tmux.fish
+++ conf.d/tmux.fish
@@ -1,5 +1,5 @@
-if not set -q TMUX
-    set -g TMUX tmux new-session -d -s tmux
-    eval $TMUX
-    tmux attach-session -d -t tmux
-end
+# if not set -q TMUX
+#     set -g TMUX tmux new-session -d -s tmux
+#     eval $TMUX
+#     tmux attach-session -d -t tmux
+# end

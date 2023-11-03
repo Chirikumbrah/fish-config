18e8d90711b03f0398c87fa4dacb0e2a27efce72
Chirikumbrah <basslo@protonmail.com>
2023-11-03 15:11:23 +0300

    added prefix for Mac OS completions

--- config.fish
+++ config.fish
@@ -7,6 +7,15 @@ function su
     command su --shell=/usr/bin/fish $argv
 end
 
+# Setup completions
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
 # Disable welcome message
 set -U fish_greeting
 set -gx LANG 'en_US.UTF-8'

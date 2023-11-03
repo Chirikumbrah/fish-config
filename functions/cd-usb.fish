4a744e1039d465b9714a9fea791af4bead67d351
Chirikumbrah <basslo@protonmail.com>
2023-11-03 15:13:09 +0300

    removed unused functions

--- functions/cd-usb.fish
+++
@@ -1,3 +0,0 @@
-function cd-usb --wraps='cd /run/media/yr/' --description 'alias cd-usb=cd /run/media/yr/'
-  cd /run/media/yr/ $argv; 
-end
--- functions/clear-screenshots.fish
+++
@@ -1,3 +0,0 @@
-function clear-screenshots --wraps='trash $YRDATA/images/screenshots/*' --description 'alias clear-screenshots=trash ~/yrData/images/screenshots/*'
-  trash $YRDATA/images/screenshots/* $argv; 
-end

6d9fa378653d0f70a957ededd64fa2c6f4a00bfd
Chirikumbrah <basslo@protonmail.com>
2023-12-12 18:15:30 +0300

    added aliases for git config list

---
+++ functions/gcfl.fish
@@ -0,0 +1,4 @@
+function gcfl --wraps='git config -l' --description 'alias gcfl=git config -l'
+  git config -l $argv
+        
+end
---
+++ functions/gcfll.fish
@@ -0,0 +1,4 @@
+function gcfll --wraps='git config --local -l' --description 'alias gcfll=git config --local -l'
+  git config --local -l $argv
+        
+end

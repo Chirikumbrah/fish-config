dceefe0749856a822729a0304ed099dba7f19d5a
Chirikumbrah <yarik.bond98@gmail.com>
2024-01-28 00:19:12 +0300

    updated gco function

--- functions/gco.fish
+++ functions/gco.fish
@@ -1,3 +1,3 @@
-function gco --wraps='git checkout' --description 'alias gco=git checkout'
-  git checkout $argv; 
+function gco --wraps='git checkout $argv || git checkout -b $argv' --description 'alias gco=git checkout $argv || git checkout -b $argv'
+  git checkout $argv || git checkout -b $argv; 
 end
